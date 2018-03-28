86c87c910b752cf5b04681c046792158b69b3208
Sagar Desai <sagar.desai@uoit.net>
2018-03-27 21:37:27 -0400

    Hooked up recent drives page to API

--- app/src/main/java/com/example/sagar/myapplication/Home_fragment.java
+++ app/src/main/java/com/example/sagar/myapplication/Home_fragment.java
@@ -6,23 +6,40 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by achal on 2018-03-07.
  */
 
 public class Home_fragment extends Fragment {
+    private Retrofit retrofit;
+    private RetrieveService retrieveService;
+
+    List<Drive> recentDrives;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
+
         Button btn = view.findViewById(R.id.startStop);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,44 +49,47 @@ public class Home_fragment extends Fragment {
             }
         });
 
-        RecyclerView recyclerView;
-        RecyclerView.Adapter mAdapter;
-        RecyclerView.LayoutManager layoutManager;
 
-        //Dummy Values
-        String date = "26032018", duration = "15";
-        int da, du;
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
 
         recyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
 
-        ArrayList<RecentDriveCard> input = new ArrayList<>();
-
-        for (int i = 0; i < 5; i++) {
-            //TODO: Connect to the API and call in date and duration for 5 recent drives
-
-            //Send in two strings date and duration
-            RecentDriveCard obj = new RecentDriveCard(date, duration);
-            input.add(obj);
-
-            //TODO: Remove when getting values from API lines 58-63
-            da = Integer.parseInt(date);
-            du = Integer.parseInt(duration);
-            da++;
-            du++;
-            date = Integer.toString(da);
-            duration = Integer.toString(du);
-
-        }// define an adapter
-
-        mAdapter = new HomeCardAdapter(input);
-        recyclerView.setAdapter(mAdapter);
-        ///////////////
+        setupRetrofit();
+        fetchRecentDrives();
 
         return view;
+    }
+
+    private void setupRetrofit() {
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl("http://" + BluetoothThread.API_ADDRESS + ":8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        this.retrieveService = retrofit.create(RetrieveService.class);
+    }
+
+    private void fetchRecentDrives() {
+        Call<List<Drive>> call = this.retrieveService.getRecentDrives();
+        call.enqueue(new Callback<List<Drive>>() {
+            @Override
+            public void onResponse(Call<List<Drive>> call, Response<List<Drive>> response) {
+                recentDrives = response.body();
+                loadDrivesIntoRecyclerView();
+            }
+
+            @Override
+            public void onFailure(Call<List<Drive>> call, Throwable t) {
+                Log.e(TAG, "Error sending message to server: " + t);
+            }
+        });
+
+    }
+
+    private void loadDrivesIntoRecyclerView() {
+        adapter = new HomeCardAdapter(recentDrives);
+        recyclerView.setAdapter(adapter);
     }
 }
