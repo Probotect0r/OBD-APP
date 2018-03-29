13d1f6ddb08dee1ca849d39311d63a3f1f321481
achal.parikh13@gmail.com <achal.parikh@uoit.net>
2018-03-28 23:06:16 -0400

    1) stop button added to dash, 2) Dash string replaced with record drive

--- app/src/main/java/com/example/sagar/myapplication/Dash_fragment.java
+++ app/src/main/java/com/example/sagar/myapplication/Dash_fragment.java
@@ -1,11 +1,13 @@
 package com.example.sagar.myapplication;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Chronometer;
 
 /**
@@ -15,6 +17,7 @@ import android.widget.Chronometer;
 public class Dash_fragment extends Fragment{
 
     private Chronometer clock;
+    private Button stop;
 
     @Nullable
     @Override
@@ -22,19 +25,19 @@ public class Dash_fragment extends Fragment{
         View view = inflater.inflate(R.layout.dash_fragment, null);
         clock = view.findViewById(R.id.chronometer2);
         startClock();
+
+        stop = view.findViewById(R.id.stopDrive);
+
+        stop.setOnClickListener(view1 -> {
+            long elapsedMills = SystemClock.elapsedRealtime() - clock.getBase();
+            clock.stop();
+            clock.setBase(SystemClock.elapsedRealtime());
+        });
+
         return view;
     }
 
     public void startClock () {
         clock.start();
     }
-
-//    public void stopDrive(View view) {
-//        //Grabs the ellapsed time in milliseconds
-//        long elapsedMills = SystemClock.elapsedRealtime() - clock.getBase();
-//        Log.d("HOME ACTIVITY STARTED", "stopDrive: " + elapsedMills);
-//        clock.stop();
-//        clock.setBase(SystemClock.elapsedRealtime());
-//    }
-
 }
