65117c47c849483021bd3020292594e89e80f47f
achal.parikh13@gmail.com <achal.parikh@uoit.net>
2018-03-28 22:37:15 -0400

    1) moved clock to dash, 2) added 3 dummy cards in the dash fragment

--- app/src/main/java/com/example/sagar/myapplication/Dash_fragment.java
+++ app/src/main/java/com/example/sagar/myapplication/Dash_fragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
 
 /**
  * Created by achal on 2018-03-07.
@@ -13,10 +14,27 @@ import android.view.ViewGroup;
 
 public class Dash_fragment extends Fragment{
 
+    private Chronometer clock;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dash_fragment, null);
+        View view = inflater.inflate(R.layout.dash_fragment, null);
+        clock = view.findViewById(R.id.chronometer2);
+        startClock();
+        return view;
     }
+
+    public void startClock () {
+        clock.start();
+    }
+
+//    public void stopDrive(View view) {
+//        //Grabs the ellapsed time in milliseconds
+//        long elapsedMills = SystemClock.elapsedRealtime() - clock.getBase();
+//        Log.d("HOME ACTIVITY STARTED", "stopDrive: " + elapsedMills);
+//        clock.stop();
+//        clock.setBase(SystemClock.elapsedRealtime());
+//    }
 
 }
